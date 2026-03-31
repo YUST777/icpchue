6afe209bac42f6a37e104242b5294ea7503a263d
YUST777 <thebox777@proton.me>
2026-03-31 19:42:31 +0000

    knip: remove 9 unused deps (crypto-js, dotenv, flatted, monaco-vim, markdown editors), delete 21 dead component files (2097 lines), clean barrel exports

--- next-app/components/mirror/editor/index.ts
+++ next-app/components/mirror/editor/index.ts
@@ -1,4 +1,1 @@
-// Editor components
 export { default as CodeWorkspace } from './CodeWorkspace';
-export { default as ComplexityModal } from './ComplexityModal';
-export { default as EditorToolbar } from './EditorToolbar';
--- next-app/components/mirror/shared/index.ts
+++
@@ -1,3 +0,0 @@
-// Shared utilities and components
-export { default as MarkdownRenderer } from './MarkdownRenderer';
-export * from './types';
